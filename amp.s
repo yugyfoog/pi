4d1cd550453448febda36c30ecfd122b8411dd30
Mark Karmadt <yugyfoog@github.com>
2020-04-23 21:43:51 -0700

    changed some names

--- amp.s
+++ amp.s
@@ -1,18 +1,18 @@
 	.align	16
-	.globl	mpint_make_zero
-mpint_make_zero:
+	.globl	mpint_zero
+mpint_zero:
 	pxor	%xmm0,%xmm0
 	movupd	%xmm0,(%rdi)
 	movsd	%xmm0,16(%rdi)
 	ret
 
 	.align	16
-	.globl	mpfloat_make_zero
-mpfloat_make_zero:
+	.globl	mpfloat_zero
+mpfloat_zero:
 	vpxor	%ymm0,%ymm0,%ymm0
 	vmovupd	%ymm0,(%rdi)
 	ret
-	
+
 
 
 	
